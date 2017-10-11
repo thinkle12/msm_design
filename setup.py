c05f6ddd3b3915787bed091a0f2c22cdeff5f908
thinkle12 <trenth12@gmail.com>
2017-10-11 16:34:46 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,14 @@ from setuptools import setup
 
 setup(
     name='msm_design',
-    version='0.5',
+    version='0.6',
     description='Markov State Modeling Protein Design Package utilizing Machine Learning and Brute Force Optimization of a specified conformational state of a protein',
     author='Trent Hinkle',
     author_email='trenth12@gmail.com',
     url='https://github.com/thinkle12/msm_design/',
     packages = ['msm_design'],
-    include_package_data = True
+    package_data = {
+    # If any package contains *.txt or *.rst files, include them:
+    '': ['*.txt', '*.pdb']
+        }
     )
