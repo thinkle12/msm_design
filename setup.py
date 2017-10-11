b516fa5cb79ee5de798e008e78b35456a72852a4
thinkle12 <trenth12@gmail.com>
2017-10-11 14:12:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,5 +14,6 @@ setup(
     author='Trent Hinkle',
     author_email='trenth12@gmail.com',
     url='https://github.com/thinkle12/msm_design/',
-    packages = ['msm_design']
+    packages = ['msm_design'],
+    data_files=[('data', ['data/*'])]
     )
