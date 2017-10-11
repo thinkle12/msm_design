74f541763539dc8ae510694a6db8d2f9993d27f3
thinkle12 <trenth12@gmail.com>
2017-10-11 11:37:48 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 10 22:52:07 2017
+
+@author: Trent
+"""
+
+from setuptools import setup
+
+setup(name='msm_design',
+      version='0.1',
+      description='Markov State Modeling Protein Design',
+      author='Trent Hinkle',
+      packages=['msm_design'],
+      zip_safe=False)
