43a7691d39beaec355911c1b630c9938c80fa5f3
thinkle12 <trenth12@gmail.com>
2017-10-11 12:45:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 setup(
     name='msm_design',
     version='0.1',
-    description='Markov State Modeling Protein Design Package utilizing Machine Learning and Brute Force Optimization of a specified conformational state of a protein,
+    description='Markov State Modeling Protein Design Package utilizing Machine Learning and Brute Force Optimization of a specified conformational state of a protein'
     author='Trent Hinkle',
     author_email='trenth12@gmail.com',
     url='https://github.com/thinkle12/msm_design/',
